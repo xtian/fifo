c78faafc1905709f2267020f071532912f4e235f
Christian Wesselhoeft <hi@xtian.us>
2019-09-08 10:45:04 -0600

    Add Dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Fifo.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        flags: [
+          :error_handling,
+          :race_conditions,
+          :unmatched_returns
+        ]
+      ]
     ]
   end
 
@@ -22,7 +29,8 @@ defmodule Fifo.MixProject do
   defp deps do
     [
       {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:credo_contrib, "~> 0.2", only: [:dev, :test], runtime: false}
+      {:credo_contrib, "~> 0.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0-rc", only: [:dev, :test], runtime: false}
     ]
   end
 end
